7d2011e1e31f0bb0372549e22c0dc85b4a9b6205
yyyzzz674 <yz624557674@163.com>
2019-10-31 20:46:04 +0800

    webpack4手动配置vue项目

---
+++ webpack4-vue/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="full-width center-content">
+    <h1>Hello World1234567</h1>
+    <hello-component :name="msg" />
+  </div>
+</template>
+<script>
+import HelloComponent from './components/HelloComponent.vue'
+export default {
+  components: {
+    HelloComponent
+  },
+  data: () => {
+    return {
+      msg: 'world123'
+    }
+  }
+}
+</script>
+<style>
+@import '../assert/app.css';
+</style>
---
+++ webpack4-vue/src/components/HelloComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <h2>Hello {{ name }}</h2>
+</template>
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  created() {
+    const arr = [1, 2, 3]
+    const arr1 = [...arr, 4]
+    alert(arr1)
+  }
+}
+</script>
+<style>
+h1 {
+  color: red;
+}
+</style>
